f928fd878a1a4cd7ae7382dca54613a6fea3667b
Finn Sheng <yupbrokendream@gmail.com>
2026-04-11 21:22:28 -0400

    fix(src): now everything is working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -19,7 +19,6 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
@@ -31,16 +30,15 @@ esp_bootloader_esp_idf::esp_app_desc!();
 #[main]
 fn main() -> ! {
     // generator version: 1.2.0
-
+    esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals = esp_hal::init(config);
 
-    info!("hello world");
-
     loop {
         let delay_start = Instant::now();
         info!("hello world");
+        while delay_start.elapsed() < Duration::from_millis(1_000) {}
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples

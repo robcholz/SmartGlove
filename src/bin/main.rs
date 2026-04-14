c21ff19db984face5c0fccbf5e01eda93c67ee35
Cedric Jovenet <cedric.jovenet0@gmail.com>
2026-04-13 19:39:52 -0500

    fix: update code style to match project rules

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -19,7 +19,7 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-// This creates a default app-descriptor required by the esp-idf bootloader.
+// This  creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
